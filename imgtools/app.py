31d27db24436d0a50d6248e261506ca2b3aeaa28
memset0 <34177126+memset0@users.noreply.github.com>
2022-10-23 22:16:39 +0800

    feat: rename

--- imgtools/app.py
+++ imgtools/app.py
@@ -11,29 +11,29 @@ with gr.Blocks(
         "[source code](https://github.com/memset0/imgtools)",
     ]))
     with gr.Tabs():
-        with gr.TabItem("灰阶图"):
+        with gr.TabItem("Grayscale"):
             with gr.Row():
                 with gr.Column():
-                    input_image = gr.Image()
+                    input_img = gr.Image()
                     submit_button = gr.Button("Submit")
                 with gr.Column():
                     output = gr.Image()
-            submit_button.click(grayscale.to_gray, inputs=input_image, outputs=output)
-            examples = gr.Examples(examples=[[asset_path("example.png")]], inputs=[input_image])
-        with gr.TabItem("灰阶图（单彩）"):
+            submit_button.click(grayscale.to_gray, inputs=input_img, outputs=output)
+            examples = gr.Examples(examples=[[asset_path("example.png")]], inputs=[input_img])
+        with gr.TabItem("Grayscale (Colored)"):
             with gr.Row():
                 input_r = gr.Textbox(label='R', value=255)
                 input_g = gr.Textbox(label='G', value=0)
                 input_b = gr.Textbox(label='B', value=0)
             with gr.Row():
                 with gr.Column():
-                    input_image = gr.Image()
+                    input_img = gr.Image()
                     submit_button = gr.Button("Submit")
                 with gr.Column():
                     output = gr.Image()
             submit_button.click(
                 grayscale.to_rgb,
-                inputs=[input_image, input_r, input_g, input_b],
+                inputs=[input_img, input_r, input_g, input_b],
                 outputs=output,
             )
-            examples = gr.Examples(examples=[[asset_path("example.png")]], inputs=[input_image])
+            examples = gr.Examples(examples=[[asset_path("example.png")]], inputs=[input_img])
